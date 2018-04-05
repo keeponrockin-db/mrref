c4b73a87610480a0aba0e23418f6bc0fb8baa4b9
gg_keeponrockin <admin@keeponrock.in>
2018-04-04 20:11:48 -0600

    Add reaction processors

--- bot.js
+++ bot.js
@@ -3,11 +3,13 @@ let monochrome = require('moodochrome-bot');
 let configFilePath = __dirname + '/config.js';
 let commandsDirectoryPath = __dirname + '/commands';
 let messageProcessorsDirectoryPath = __dirname + '/message_processors';
+let reactionProcessorsDirectoryPath = __dirname + '/reaction_processors';
 let settingsFilePath = __dirname + '/server_settings.js';
 
 let bot = new monochrome(
   configFilePath,
   commandsDirectoryPath,
   messageProcessorsDirectoryPath,
+  reactionProcessorsDirectoryPath,
   settingsFilePath);
 bot.connect();
