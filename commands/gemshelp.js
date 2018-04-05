ceabaaf3285ba17dfe60a9746d044bd282459548
gg_keeponrockin <admin@keeponrock.in>
2018-04-05 06:30:53 -0600

    added help command and renamed headers to header

---
+++ commands/gemshelp.js
@@ -0,0 +1,20 @@
+'use strict'
+
+/**
+* Demonstrates persisting data.
+*/
+module.exports = {
+  commandAliases: ['$gemshelp'],
+  canBeChannelRestricted: true,
+  uniqueId: 'gemshelp13857389',
+  serverAdminOnly: false,
+  shortDescription: 'help for gems',
+  usageExample: '$gems Steam Xrd',
+  action(bot, msg, suffix) {
+    let message = 'Type ``$gems`` to start a room.\r\n';
+    message += 'To list a specific game, type the name of the game after. Eg. ```$gems rev2```\r\n';
+    message += 'To add a search id, password or some other misc info, after starting a room, use ``$gemcode`` followed by the code. Eg. ```$gemcode j8c7```\r\n';
+    message += 'To add an image to specific room names, use ``$gemheader`` followed by a link to the image. Eg. ```$gemheader https://steamcdn-a.akamaihd.net/steam/apps/631560/header.jpg```\r\n';
+    return msg.channel.createMessage(message);
+  }
+};
