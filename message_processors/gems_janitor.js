363a1fef703d4822992364083ba5024503351248
gg_keeponrockin <admin@keeponrock.in>
2018-04-07 00:19:24 -0600

    Delete non-command messages in gemslist channel

---
+++ message_processors/gems_janitor.js
@@ -0,0 +1,24 @@
+'use strict'
+const persistence = require('moodochrome-bot').persistence;
+
+/**
+* Adds bot uploaded comics to an archive
+*/
+module.exports = {
+  name: 'gemsjanitor',
+  action(bot, msg) {
+    const gemsList = require('./../gems_list.js');
+    let user = msg.channel.guild.members.find(user => user.id === msg.author.id);
+    let role = msg.channel.guild.roles.find(role => role.name === 'Mr. Referee');
+    let isMrRef = user.roles.find(roleId => roleId === role.id);
+    if (isMrRef) {
+      return false;
+    }
+
+    return gemsList.getChannel(msg.channel.guild).then(channel => {
+      if (msg.channel.id === channel.id) {
+        channel.deleteMessage(msg.id);
+      }
+    });
+  }
+};
